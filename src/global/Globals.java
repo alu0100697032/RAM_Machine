ac4873de27deecbaa28bdfb57a81022ec8f91756
Víctor Hernández <alu0100697032@ull.edu.es>
2016-02-16 20:45:15 +0000

    Added some global variables (Globals class)

---
+++ src/global/Globals.java
@@ -0,0 +1,13 @@
+/**
+ * File name:Globals.java
+ * Package name: global
+ * Proyect name: daa_ramMachine
+ */
+package global;
+
+public class Globals {
+	public static final int IMMEDIATE_ADDRESSING = 0;
+	public static final int REFERENCE_ADDRESSING = 1;
+	public static final int INDIRECT_ADDRESSING = 2;
+	public static final int JUMP_ADDRESSING = 3;
+}
