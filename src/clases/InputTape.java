1b3a5e9192c84b0474599b16206de647eacb2592
Víctor Hernández <alu0100697032@ull.edu.es>
2016-02-18 11:12:01 +0000

    InputTape modified

--- src/clases/InputTape.java
+++ src/clases/InputTape.java
@@ -12,12 +12,14 @@ public class InputTape {
 	 * Atributes
 	 */
 	private ArrayList<Integer> elements;
+	private int pointer;
 	
 	/**
 	 * Constructor: InputTape
 	 */
 	public InputTape(ArrayList<Integer> elements) {
 		setElements(elements);
+		pointer = 0;
 	}
 	
 	/**
@@ -41,11 +43,31 @@ public class InputTape {
 	public int getElement(int position) {
 		return elements.get(position);
 	}
-	
+	/**
+	 * getCurrentPointerElement
+	 */
+	public int getCurrentPointerElement() {
+		return elements.get(pointer);
+	}
 	/**
 	 * @param elementosCinta the elementosCinta to set
 	 */
 	public void setElements(ArrayList<Integer> elements) {
 		this.elements = elements;
+	}
+
+	/**
+	 * @return the pointer
+	 */
+	public int getPointer() {
+		int aux = pointer;
+		return aux;
+	}
+
+	/**
+	 * @param pointer the pointer to set
+	 */
+	public void incrementPointer() {
+		pointer++;
 	}
 }
